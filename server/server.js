47cd582c0ede6da108cabaa18a546420d4963a31
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-17 11:00:29 +0200

    Create start server

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const morgan = require("morgan");
+const dotenv = require("dotenv").config();
+const { connectDB } = require("./config/db");
+
+const PORT = process.env.PORT || 4000;
+
+async function startServer() {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app: app });
+
+  app.use(
+    fileUpload({
+      createParentPath: true,
+    })
+  );
+
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(morgan("dev"));
+
+  app.use((req, res) => {
+    res.send("Hello from server");
+  });
+
+  connectDB();
+
+  app.listen(PORT, () => console.log("Server running on port 4000"));
+}
+
+startServer();
